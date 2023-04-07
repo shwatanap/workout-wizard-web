dec2ca2fff655b281144f3ba3561cf4c4d8c75c6
TanakaDaishi0806 <dai4dai0806@ezweb.ne.jp>
2023-04-07 16:30:55 +0900

    Tailwindcssのカラーパレットを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,22 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      yellow: {
+        DEFAULT: '#ffff00'
+      },
+      black: {
+        DEFAULT: '#333333'
+      },
+      white: {
+        DEFAULT: '#ffffff'
+      },
+      gray: {
+        DEFAULT: '#D9D9D9'
+      }
+    }
+
+
   },
   plugins: [],
 };
