c56797c505d31af1abead0761825dbb57c6463b3
TanakaDaishi0806 <dai4dai0806@ezweb.ne.jp>
2023-04-08 17:46:24 +0900

    ロゴが中心に近づくように変更

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
     return (
         <div className="bg-yellow h-[50px] flex">
-            <div className="text-black font-bold text-[20px] my-auto mx-auto ">Workout Wizard</div>
-            <div className="text-[12px] my-auto mr-[10px]">2023 kyutechjapan - @HomePage</div>
+            <div className="text-black font-bold text-[15px] my-auto mx-auto ">Workout Wizard</div>
+            <div className="text-[1px] my-auto mr-[1px]">2023 kyutechjapan <br />- @HomePage</div>
         </div>
     );
 };
