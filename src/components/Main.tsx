e3c4af612ab6514c88660a4585653da805898d33
TanakaDaishi0806 <dai4dai0806@ezweb.ne.jp>
2023-04-08 17:45:49 +0900

    インポートとimgタグ内のalt属性の追加、ボタンパーツの汎用性を追加した

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,12 +1,8 @@
-import React from "react";
-import { Workout } from '@/types/workout'
-import { dummyproceedureData } from "@/datas/dummyproceedureData";
-import Bt from '@/parts/Bt'
-import Box from "@/parts/box";
+import { loginbuttonData } from "@/datas/loginbuttonData";
+import BaseButton from '@/parts/BaseButton';
+import Explain from "./Explain";
 
-type Props = {
-    workout: Workout;
-}
+
 
 
 const Main = () => {
@@ -14,27 +10,21 @@ const Main = () => {
     return (
         <div>
             <div>
-                <div className="bg-black h-[770px] mt-[5px]">
+                <div className="bg-black h-[770px]">
                     <div className="pt-[80px] w-[300px] h-[300px] mx-auto">
-                        <img className="" src='/img/logo.png' />
+                        <img src='/img/logo.png' alt="WorkoutWizardのロゴ" />
                     </div>
                     <div className="text-[16px] text-white w-[250px] pt-[100px] mx-auto">
                         <span className="text-yellow font-bold">Workout Wizard</span>は筋トレサポートサービスです。<br />あなたを三日坊主とは誰にも言わせない。
                     </div>
-                    <Bt />
+                    <BaseButton
+                        button={loginbuttonData}
+                    />
                     <div className='text-white text-center mt-[20px]'><span className='border-b'>新規登録</span></div>
 
                 </div>
-                <div className="bg-gray h-[1015px]">
-                    <div className="text-center text-black font-bold text-[36px] pt-[40px]">How To Use</div>
-                    {dummyproceedureData.map((Items) => {
-                        return (
-                            <Box
-                                workout={Items}
-                            />
-                        );
-                    })}
-                </div>
+                <Explain />
+
             </div>
         </div>
     );
