fda9b9e454b5fdee2c19e4d862ec87d6ce484757
TanakaDaishi0806 <dai4dai0806@ezweb.ne.jp>
2023-04-09 12:23:36 +0900

    functinal componetに修正

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -2,7 +2,7 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Main from "@/components/Main";
 
-function Start() {
+const Start = () => {
   return (
     <div>
       <Header />
@@ -10,6 +10,6 @@ function Start() {
       <Footer />
     </div>
   );
-}
+};
 
 export default Start;
