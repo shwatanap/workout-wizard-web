7a8c0bf5de7d60247965d01dfd08f42b0699833d
TanakaDaishi0806 <dai4dai0806@ezweb.ne.jp>
2023-04-09 11:15:40 +0900

    Boxコンポーネント名をProcedureBoxに変更

--- src/components/Explain.tsx
+++ src/components/Explain.tsx
@@ -1,13 +1,13 @@
-import Box from "@/parts/box";
-import { dummyprocedureData } from "@/datas/dummyprocedureData";
+import ProcedureBox from "@/parts/ProcedureBox";
+import { dummyProcedureData } from "@/datas/dummyProcedureData";
 
 const Explain = () => {
     return (
         <div className="bg-gray h-[1015px]">
             <div className="text-center text-black font-bold text-[36px] pt-[40px]">How To Use</div>
-            {dummyprocedureData.map((item) => {
+            {dummyProcedureData.map((item) => {
                 return (
-                    <Box
+                    <ProcedureBox
                         procedure={item}
                     />
                 );
