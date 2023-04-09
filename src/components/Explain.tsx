a4345de86ad393329be78c71ec0b97c756c367b4
TanakaDaishi0806 <dai4dai0806@ezweb.ne.jp>
2023-04-09 12:24:00 +0900

    map処理に型を追加

--- src/components/Explain.tsx
+++ src/components/Explain.tsx
@@ -1,11 +1,12 @@
 import ProcedureBox from "@/parts/ProcedureBox";
 import { dummyProcedureData } from "@/datas/dummyProcedureData";
+import { Procedure } from "@/types/workout";
 
 const Explain = () => {
   return (
     <div className="bg-gray h-[1015px]">
       <div className="text-center text-black font-bold text-[36px] pt-[40px]">How To Use</div>
-      {dummyProcedureData.map((item) => {
+      {dummyProcedureData.map((item: Procedure) => {
         return <ProcedureBox procedure={item} />;
       })}
     </div>
