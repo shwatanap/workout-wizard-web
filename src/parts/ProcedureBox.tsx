1d66cea4ca7b2f171579359578aa174936831f3a
TanakaDaishi0806 <dai4dai0806@ezweb.ne.jp>
2023-04-09 11:19:56 +0900

    コンポーネント名をbox⇒ProcedureBoxに変更

--- src/parts/ProcedureBox.tsx
+++ src/parts/ProcedureBox.tsx
@@ -2,12 +2,11 @@ import React from "react";
 
 import { Procedure } from "@/types/workout";
 
-
 type Props = {
     procedure: Procedure;
 };
 
-const Box: React.FC<Props> = ({ procedure }) => {
+const ProcedureBox: React.FC<Props> = ({ procedure }) => {
     return (
         <div className="bg-white mt-[20px] mx-[10px]">
             <div className="border-t border-b pt-[5px] pb-[5px] pl-[5px]">{procedure.section}</div>
@@ -19,4 +18,4 @@ const Box: React.FC<Props> = ({ procedure }) => {
     );
 };
 
-export default Box;
+export default ProcedureBox;
