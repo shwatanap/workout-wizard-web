3af490f24f77d710f7442d1733b7ccc2ca33ca5c
TanakaDaishi0806 <dai4dai0806@ezweb.ne.jp>
2023-04-09 23:11:54 +0900

    UIをStartTemplateにまとめStartではロジックを書くように変更

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -1,15 +1,7 @@
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
-import Main from "@/components/Main";
+import StartTemplate from "@/templates/StartTemplate";
 
 const Start = () => {
-  return (
-    <div>
-      <Header />
-      <Main />
-      <Footer />
-    </div>
-  );
+  return <StartTemplate />;
 };
 
 export default Start;
---
+++ src/templates/StartTemplate.tsx
@@ -0,0 +1,31 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Explain from "@/components/Explain";
+import BaseButton from "@/parts/base/BaseButton";
+
+const StartTemplate = () => {
+  return (
+    <div>
+      <Header />
+      <div className="bg-black h-[770px]">
+        <div className="pt-[80px] w-[300px] h-[300px] mx-auto">
+          <img src="/img/logo.png" alt="WorkoutWizardのロゴ" />
+        </div>
+        <div className="text-[16px] text-white w-[250px] pt-[100px] mx-auto">
+          <span className="text-yellow font-bold">Workout Wizard</span>
+          は筋トレサポートサービスです。
+          <br />
+          あなたを三日坊主とは誰にも言わせない。
+        </div>
+        <BaseButton text="ログイン" textColor="black" backgroundColor="yellow" />
+        <div className="text-white text-center mt-[20px]">
+          <span className="border-b">新規登録</span>
+        </div>
+      </div>
+      <Explain />
+      <Footer />
+    </div>
+  );
+};
+
+export default StartTemplate;
