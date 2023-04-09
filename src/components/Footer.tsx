674ce444a9dac231772445225fdc9a6debd74c4c
TanakaDaishi0806 <dai4dai0806@ezweb.ne.jp>
2023-04-09 23:12:56 +0900

    ロゴを中心に配置

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,10 @@
 const Footer = () => {
   return (
-    <div className="bg-yellow h-[50px] flex">
-      <div className="text-black font-bold text-[15px] my-auto mx-auto ">Workout Wizard</div>
-      <div className="text-[1px] my-auto mr-[1px]">
+    <div className="bg-yellow h-[50px] relative my-auto">
+      <div className="text-black font-bold text-[15px] w-[390px] text-center absolute mt-[12px]">
+        Workout Wizard
+      </div>
+      <div className="text-[1px] my-auto mr-[1px] text-right absolute w-[390px] mt-[12px]">
         2023 kyutechjapan <br />- @HomePage
       </div>
     </div>
