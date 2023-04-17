6cecf7a9d79f864fa7427af131c2bc2bec115ce1
kohki miyama <kmx.111010@gmail.com>
2023-04-17 11:00:24 +0900

    :sparkles: POSTリクエストの処理を追加

--- src/templates/InputTemplate.tsx
+++ src/templates/InputTemplate.tsx
@@ -1,11 +1,33 @@
+import axios from "axios";
 import React from "react";
 import { Link } from "react-router-dom";
 
-const InputTemplate = () => {
+type Props = {
+  part: string;
+  time: string;
+  intensity: string;
+};
+
+const InputTemplate: React.FC<Props> = ({ part, time, intensity }) => {
+  // TODO 変数の渡し方？
+  const createNewTraining = () => {
+    axios
+      .post("", {
+        part: part,
+        time: time,
+        intensity: intensity,
+      })
+      .then((response) => {})
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div>
       <Link to="/output">
-        <button>トレーニング開始！</button>
+        {/* <BaseButton text="トレーニング開始" textColor="black" backgroundColor="yellow" /> */}
+        <button onClick={createNewTraining}>トレーニング開始</button>
       </Link>
     </div>
   );
