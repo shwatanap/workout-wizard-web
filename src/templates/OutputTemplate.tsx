f4bf579de12461a96281413209c4d943566f0c2e
kohki miyama <kmx.111010@gmail.com>
2023-04-17 15:07:45 +0900

    :sparkles: postリクエストのひな形作成

--- src/templates/OutputTemplate.tsx
+++ src/templates/OutputTemplate.tsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import axios from "axios";
+import { error } from "console";
 
 const OutputTemplate = () => {
+  useEffect(() => {
+    axios
+      .post("", {})
+      .then((response) => {})
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+
   return <div>OutputTemplate</div>;
 };
 
