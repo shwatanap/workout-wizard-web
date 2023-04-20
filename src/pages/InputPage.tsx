08bda4e42db494a1f3e2abdda5365655e32a362c
kohki miyama <kmx.111010@gmail.com>
2023-04-20 17:36:14 +0900

    :sparkles: ロジックの作成#6

--- src/pages/InputPage.tsx
+++ src/pages/InputPage.tsx
@@ -2,53 +2,61 @@ import React, { useState } from "react";
 
 import InputTemplate from "@/templates/InputTemplate";
 
+import { InputInfo } from "@/types/input";
+
 const InputPage = () => {
   // ここで定義すべき？
   const [part, setPart] = useState("");
   const [time, setTime] = useState("");
   const [intensity, setIntensity] = useState("normal");
+  // const [menu, setMenu] = useState<InputInfo>();
 
-  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+  // const [inputValue, setInputValue] = useState("");
+  // const [menu, setMenu] = useState<InputInfo[]>([]);
+
+  const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const { name, value } = e.target;
     switch (name) {
-      case "part":
-        setPart(value);
-        break;
+      // case "part":
+      //   setPart(value);
+      //   break;
       case "time":
         setTime(value);
+        break;
+      case "intensity":
+        setIntensity(value);
         break;
       default:
         break;
     }
+    console.log("part : ", part);
+    console.log("time : ", time);
+    console.log("intensity : ", intensity);
+    console.log("value : ", value);
   };
 
-  const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setIntensity(e.target.value);
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPart(e.target.value);
+    console.log("part : ", part);
+    console.log("time : ", time);
+    console.log("intensity : ", intensity);
+    console.log("value : ", e.target.value);
+  };
+
+  const handleCreateMenu = () => {
+    console.log("part : ", part);
+    console.log("time : ", time);
+    console.log("intensity : ", intensity);
   };
 
   return (
     <div>
-      <form>
-        <label>
-          部位
-          <input name="part" type="text" value={part} onChange={handleInput} />
-        </label>
-        <br />
-        <label>
-          時間
-          <input name="time" type="number" value={time} onChange={handleInput} />
-        </label>
-        <br />
-        <label>
-          強度
-          <select value={intensity} onChange={handleSelect}>
-            <option value="high">高い</option>
-            <option value="normal">普通</option>
-            <option value="low">低い</option>
-          </select>
-        </label>
-      </form>
-      <InputTemplate part={part} time={time} intensity={intensity} />
+      {/* <InputTemplate part={part} time={time} intensity={intensity} handleSelect={handleSelect} /> */}
+      <InputTemplate
+        handleInput={handleInput}
+        handleSelect={handleSelect}
+        handleCreateMenu={handleCreateMenu}
+      />
     </div>
   );
 };
