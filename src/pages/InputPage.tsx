43b86608a4640eff70f9ffd4efba1767f0ce3017
kohki miyama <kmx.111010@gmail.com>
2023-04-23 13:54:33 +0900

    :adhesive_bandage: コメントアウトを削除

--- src/pages/InputPage.tsx
+++ src/pages/InputPage.tsx
@@ -2,24 +2,14 @@ import React, { useState } from "react";
 
 import InputTemplate from "@/templates/InputTemplate";
 
-import { InputInfo } from "@/types/input";
-
 const InputPage = () => {
-  // ここで定義すべき？
   const [part, setPart] = useState("");
   const [time, setTime] = useState("");
   const [intensity, setIntensity] = useState("normal");
-  // const [menu, setMenu] = useState<InputInfo>();
-
-  // const [inputValue, setInputValue] = useState("");
-  // const [menu, setMenu] = useState<InputInfo[]>([]);
 
   const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const { name, value } = e.target;
     switch (name) {
-      // case "part":
-      //   setPart(value);
-      //   break;
       case "time":
         setTime(value);
         break;
@@ -29,21 +19,17 @@ const InputPage = () => {
       default:
         break;
     }
-    console.log("part : ", part);
-    console.log("time : ", time);
-    console.log("intensity : ", intensity);
-    console.log("value : ", value);
   };
 
   const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPart(e.target.value);
-    console.log("part : ", part);
-    console.log("time : ", time);
-    console.log("intensity : ", intensity);
-    console.log("value : ", e.target.value);
   };
 
-  const handleCreateMenu = () => {
+  // 入力のバリデーションチェック
+  const handleCheckInput = () => {};
+
+  const handleCreateMenu = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    console.log("####################");
     console.log("part : ", part);
     console.log("time : ", time);
     console.log("intensity : ", intensity);
@@ -51,7 +37,6 @@ const InputPage = () => {
 
   return (
     <div>
-      {/* <InputTemplate part={part} time={time} intensity={intensity} handleSelect={handleSelect} /> */}
       <InputTemplate
         handleInput={handleInput}
         handleSelect={handleSelect}
