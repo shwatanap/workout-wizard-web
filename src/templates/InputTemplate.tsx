102c9cb70a1fd67032f138c2810bafac4049a751
kohki miyama <kmx.111010@gmail.com>
2023-04-24 21:55:34 +0900

    :sparkles: トレーニング時間のUI作成#6

--- src/templates/InputTemplate.tsx
+++ src/templates/InputTemplate.tsx
@@ -1,53 +1,79 @@
 import React from "react";
-import { Link } from "react-router-dom";
+
+import { Container, Grid } from "@mui/material";
+import { Typography } from "@mui/material";
+import { Slider } from "@mui/material";
 
 import NextButton from "@/parts/input/NextButton";
+// import { InputInfo } from "@/types/input";
 
 type Props = {
   handleInput: (e: React.ChangeEvent<HTMLInputElement>) => void;
   handleSelect: (e: React.ChangeEvent<HTMLSelectElement>) => void;
-  handleCreateMenu: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  handleCreateMenu: (e: React.MouseEvent<HTMLInputElement, MouseEvent>) => void;
 };
+
+const times = [
+  { value: 10, label: "10分" },
+  { value: 15, label: "15分" },
+  { value: 20, label: "20分" },
+  { value: 25, label: "25分" },
+  { value: 30, label: "30分" },
+];
 
 const InputTemplate: React.FC<Props> = ({ handleInput, handleSelect, handleCreateMenu }) => {
   return (
     <div>
-      <form>
-        <label>
-          部位
-          <input type="radio" name="part" value="pectoral" onChange={handleInput} />
-          胸筋
-          <input type="radio" name="part" value="shoulder" onChange={handleInput} />
-          肩
-          <input type="radio" name="part" value="upper" onChange={handleInput} />
-          上半身
-          <input type="radio" name="part" value="lower" onChange={handleInput} />
-          下半身
-          <input type="radio" name="part" value="whole" onChange={handleInput} />
-          全身
-        </label>
-        <br />
-        <label>
-          時間
-          <select name="time" onChange={handleSelect}>
-            <option value="10">10</option>
-            <option value="15">15</option>
-            <option value="20">20</option>
-            <option value="25">25</option>
-            <option value="30">30</option>
-          </select>
-        </label>
-        <br />
-        <label>
-          強度
-          <select name="intensity" onChange={handleSelect}>
-            <option value="high">高い</option>
-            <option value="normal">普通</option>
-            <option value="low">低い</option>
-          </select>
-        </label>
-      </form>
-      <NextButton path="/output" name="トレーニング開始" handleCreateMenu={handleCreateMenu} />
+      <Container>
+        <form>
+          <label>
+            部位
+            <input type="radio" name="part" value="pectoral" />
+            胸筋
+            <input type="radio" name="part" value="shoulder" />
+            肩
+            <input type="radio" name="part" value="upper" />
+            上半身
+            <input type="radio" name="part" value="lower" />
+            下半身
+            <input type="radio" name="part" value="whole" />
+            全身
+          </label>
+          <br />
+          <div>
+            <Grid container direction="column" alignItems="center">
+              <Grid item>
+                <Typography id="time-slider" gutterBottom>
+                  トレーニング時間
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Slider
+                  sx={{ width: 250 }}
+                  min={10}
+                  max={30}
+                  size="medium"
+                  track={false}
+                  aria-label="time-slider"
+                  step={5}
+                  defaultValue={20}
+                  marks={times}
+                />
+              </Grid>
+            </Grid>
+          </div>
+          <br />
+          <label>
+            強度
+            <select>
+              <option value="high">高い</option>
+              <option value="normal">普通</option>
+              <option value="low">低い</option>
+            </select>
+          </label>
+        </form>
+        <NextButton path="/output" name="トレーニング開始" handleCreateMenu={handleCreateMenu} />
+      </Container>
     </div>
   );
 };
