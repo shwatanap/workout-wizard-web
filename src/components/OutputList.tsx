4a2e66fb61ff9d1b3739cf43bc5f89a4e6fae6ee
TanakaDaishi0806 <dai4dai0806@ezweb.ne.jp>
2023-04-25 13:13:00 +0900

    materialuiのTimelineを 使ってデザインの変更

--- src/components/OutputList.tsx
+++ src/components/OutputList.tsx
@@ -1,14 +1,26 @@
+import * as React from "react";
+
+import Timeline from "@mui/lab/Timeline";
+
 import { dummyOutputlistData } from "@/datas/dummyOutputlistData";
 import { Output } from "@/types/workout";
 import OutputBox from "@/parts/output/OutputBox";
 
 const OutputList = () => {
   return (
-    <div className="bg-black pb-[30px]">
-      {dummyOutputlistData.map((item: Output, index: number) => (
-        <OutputBox outputlist={item} index={index} />
-      ))}
-    </div>
+    <Timeline
+      sx={{
+        bgcolor: "#333333",
+        marginLeft: "-80%",
+        width: "180%",
+        color: "white",
+        paddingBottom: "50px",
+      }}
+    >
+      {dummyOutputlistData.map((item: Output, index) => {
+        return <OutputBox outputlist={item} />;
+      })}
+    </Timeline>
   );
 };
 
--- src/parts/output/OutputBox.tsx
+++ src/parts/output/OutputBox.tsx
@@ -1,28 +1,34 @@
 import React from "react";
 
+import TimelineItem from "@mui/lab/TimelineItem";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineContent from "@mui/lab/TimelineContent";
+import TimelineDot from "@mui/lab/TimelineDot";
+import Typography from "@mui/material/Typography";
+
 import { Output } from "@/types/workout";
 
 type Props = {
   outputlist: Output;
-  index: number;
 };
 
-const OutputBox: React.FC<Props> = ({ outputlist, index }) => {
+const OutputBox: React.FC<Props> = ({ outputlist }) => {
   return (
-    <div className="pl-[20px] pt-[10px] pr-[40px] pb-[10px]">
-      <div className="flex">
-        <div className="h-[30px] w-[30px] rounded-full bg-yellow text-black flex justify-center items-center">
-          {index + 1}
-        </div>
-
-        <div className="flex items-center ml-[10px] text-white text-[18px] font-semibold">
+    <TimelineItem>
+      <TimelineSeparator>
+        <TimelineDot />
+        <TimelineConnector />
+      </TimelineSeparator>
+      <TimelineContent>
+        <Typography variant="h6" component="span">
           {outputlist.label}
-        </div>
-      </div>
-      <div className="ml-[14px] mt-[10px] pl-[25px] pb-[50px] border-l-[1px] border-gray text-white text-[16px] tracking-wider">
-        {outputlist.description}
-      </div>
-    </div>
+        </Typography>
+        <Typography sx={{ marginTop: "10px", marginBottom: "50px" }}>
+          {outputlist.description}
+        </Typography>
+      </TimelineContent>
+    </TimelineItem>
   );
 };
 
