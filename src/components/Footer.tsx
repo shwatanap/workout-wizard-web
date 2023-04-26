eb200ca6013eea0578361642a44d4eb77ee49047
TanakaDaishi0806 <dai4dai0806@ezweb.ne.jp>
2023-04-26 11:45:57 +0900

    ＃数字を付け忘れたのと,コメント部分のいらないコードを削除#10

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,5 @@
 import { Typography, Grid } from "@mui/material";
 
-// const Footer1= () => {
-//   return (
-//     <div className="bg-yellow h-[50px] relative my-auto">
-//       <div className="text-black font-bold text-[15px] w-[390px] text-center absolute mt-[12px]">
-//         Workout Wizard
-//       </div>
-//       <div className="text-[1px] my-auto mr-[1px] text-right absolute w-[390px] mt-[12px]">
-//         2023 kyutechjapan <br />- @HomePage
-//       </div>
-//     </div>
-//   );
-// };
-
 const Footer = () => {
   return (
     <Grid
