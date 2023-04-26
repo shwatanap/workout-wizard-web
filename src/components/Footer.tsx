314266f88d18c589bee04f064820d8986c560bb0
TanakaDaishi0806 <dai4dai0806@ezweb.ne.jp>
2023-04-26 11:43:05 +0900

    Footerコンポーネントをmaterialuiに変更

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,36 @@
+import { Typography, Grid } from "@mui/material";
+
+// const Footer1= () => {
+//   return (
+//     <div className="bg-yellow h-[50px] relative my-auto">
+//       <div className="text-black font-bold text-[15px] w-[390px] text-center absolute mt-[12px]">
+//         Workout Wizard
+//       </div>
+//       <div className="text-[1px] my-auto mr-[1px] text-right absolute w-[390px] mt-[12px]">
+//         2023 kyutechjapan <br />- @HomePage
+//       </div>
+//     </div>
+//   );
+// };
+
 const Footer = () => {
   return (
-    <div className="bg-yellow h-[50px] relative my-auto">
-      <div className="text-black font-bold text-[15px] w-[390px] text-center absolute mt-[12px]">
-        Workout Wizard
-      </div>
-      <div className="text-[1px] my-auto mr-[1px] text-right absolute w-[390px] mt-[12px]">
-        2023 kyutechjapan <br />- @HomePage
-      </div>
-    </div>
+    <Grid
+      container
+      spacing={1}
+      sx={{ bgcolor: "#ffff00", color: "#333333", pt: "15px", pb: "15px" }}
+    >
+      <Grid xs={2}></Grid>
+      <Grid xs={8}>
+        <Typography variant="h6" sx={{ textAlign: "center", fontWeight: "600" }}>
+          Workout Wizard
+        </Typography>
+      </Grid>
+      <Grid xs={2}>
+        <Typography sx={{ fontSize: "1px" }}>2023 kyutech </Typography>
+        <Typography sx={{ fontSize: "1px" }}>-@HomePage</Typography>
+      </Grid>
+    </Grid>
   );
 };
 
