707ead7b1739b654c7edb788cb68c0e51a216abb
kohki miyama <kmx.111010@gmail.com>
2023-04-27 16:45:54 +0900

    :sparkles: 部位項目入力のUI作成

--- src/templates/InputTemplate.tsx
+++ src/templates/InputTemplate.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 
-import { Radio, RadioGroup } from "@mui/material";
+import { Radio, RadioGroup, SelectChangeEvent } from "@mui/material";
 import { Box, ButtonGroup, Button } from "@mui/material";
+import { InputLabel } from "@mui/material";
+import { Select } from "@mui/material";
+import { MenuItem } from "@mui/material";
 import { FormControlLabel } from "@mui/material";
 import { FormControl } from "@mui/material";
 import { FormLabel } from "@mui/material";
@@ -14,9 +17,17 @@ import NextButton from "@/parts/input/NextButton";
 
 type Props = {
   handleInput: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  handleSelect: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+  handleSelect: (e: SelectChangeEvent) => void;
   handleCreateMenu: (e: React.MouseEvent<HTMLInputElement, MouseEvent>) => void;
 };
+
+const parts = [
+  { value: "pectoral", label: "胸筋" },
+  { value: "shoulder", label: "肩" },
+  { value: "upper", label: "上半身" },
+  { value: "lower", label: "下半身" },
+  { value: "whole", label: "全身" },
+];
 
 const times = [
   { value: 10, label: "10分" },
@@ -32,99 +43,93 @@ const intensitys = [
   { value: 2, label: "高い" },
 ];
 
-const parts = ["胸筋", "肩", "上半身", "下半身", "全身"];
-
 const InputTemplate: React.FC<Props> = ({ handleInput, handleSelect, handleCreateMenu }) => {
   return (
-    <Container sx={{ backgroundColor: "#333333" }}>
-      <Typography
-        variant="h6"
-        style={{ color: "white", marginTop: "10px", textAlign: "center" }}
-        gutterBottom
-      >
-        トレーニングメニュー
-      </Typography>
-      {/* <ButtonGroup variant="contained" aria-label="outlined primary button group">
-        <Grid container spacing={1}>
-          {parts.map((option) => (
-            <Grid xs={2}>
-              <Button key={option}>{option}</Button>
-            </Grid>
-          ))}
-        </Grid>
-      </ButtonGroup>
-
-      <FormControl>
-        <Typography variant="h6" id="parts-slider" gutterBottom>
+    <Box sx={{ backgroundColor: "#333333" }}>
+      <Container maxWidth="xs">
+        <Typography
+          variant="h6"
+          style={{ color: "white", padding: "10px", textAlign: "center" }}
+          gutterBottom
+        >
+          トレーニングメニュー
+        </Typography>
+        <Typography
+          variant="h6"
+          id="parts-slider"
+          style={{ marginTop: "20%", color: "white" }}
+          gutterBottom
+        >
           トレーニング部位
         </Typography>
-        <RadioGroup row defaultValue="pectoral">
-          <FormControlLabel value="pectoral" control={<Radio />} label="胸筋" />
-          <FormControlLabel value="shoulder" control={<Radio />} label="肩" />
-          <FormControlLabel value="upper" control={<Radio />} label="上半身" />
-          <FormControlLabel value="lower" control={<Radio />} label="下半身" />
-          <FormControlLabel value="whole" control={<Radio />} label="全身" />
-        </RadioGroup>
-      </FormControl> */}
+        <FormControl fullWidth>
+          <InputLabel id="part-select-label">部位</InputLabel>
+          <Select labelId="part-select-label" id="part-select" label="part" onChange={handleSelect}>
+            {parts.map((part) => (
+              <MenuItem value={part.value}>{part.label}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
 
-      <Grid container direction="column" alignItems="center">
-        <Grid item>
-          <Typography
-            variant="h6"
-            id="time-slider"
-            style={{ marginTop: "20%", color: "white" }}
-            gutterBottom
-          >
-            トレーニング時間
-          </Typography>
+        <Grid container direction="column" alignItems="center">
+          <Grid item>
+            <Typography
+              variant="h6"
+              id="time-slider"
+              style={{ marginTop: "20%", color: "white" }}
+              gutterBottom
+            >
+              トレーニング時間
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Slider
+              sx={{ width: 250, color: "#ffff00" }}
+              min={10}
+              max={30}
+              size="medium"
+              track={false}
+              aria-label="time-slider"
+              step={5}
+              defaultValue={20}
+              marks={times.map((time) => ({
+                value: time.value,
+                label: <Typography sx={{ color: "white" }}>{time.label}</Typography>,
+              }))}
+            />
+          </Grid>
         </Grid>
-        <Grid item>
-          <Slider
-            sx={{ width: 250, color: "#ffff00" }}
-            min={10}
-            max={30}
-            size="medium"
-            track={false}
-            aria-label="time-slider"
-            step={5}
-            defaultValue={20}
-            marks={times.map((time) => ({
-              value: time.value,
-              label: <Typography sx={{ color: "white" }}>{time.label}</Typography>,
-            }))}
-          />
+        <Grid container direction="column" alignItems="center">
+          <Grid item>
+            <Typography
+              variant="h6"
+              id="intensity-slider"
+              style={{ marginTop: "20%", color: "white" }}
+              gutterBottom
+            >
+              トレーニング強度
+            </Typography>
+          </Grid>
+          <Grid>
+            <Slider
+              sx={{ width: 250, color: "#ffff00" }}
+              min={0}
+              max={2}
+              size="medium"
+              track={false}
+              aria-label="intensity-slider"
+              step={1}
+              defaultValue={1}
+              marks={intensitys.map((intensity) => ({
+                value: intensity.value,
+                label: <Typography sx={{ color: "white" }}>{intensity.label}</Typography>,
+              }))}
+            />
+          </Grid>
         </Grid>
-      </Grid>
-      <Grid container direction="column" alignItems="center">
-        <Grid item>
-          <Typography
-            variant="h6"
-            id="intensity-slider"
-            style={{ marginTop: "20%", color: "white" }}
-            gutterBottom
-          >
-            トレーニング強度
-          </Typography>
-        </Grid>
-        <Grid>
-          <Slider
-            sx={{ width: 250, color: "#ffff00" }}
-            min={0}
-            max={2}
-            size="medium"
-            track={false}
-            aria-label="intensity-slider"
-            step={1}
-            defaultValue={1}
-            marks={intensitys.map((intensity) => ({
-              value: intensity.value,
-              label: <Typography sx={{ color: "white" }}>{intensity.label}</Typography>,
-            }))}
-          />
-        </Grid>
-      </Grid>
-      <NextButton path="/output" name="トレーニング開始" handleCreateMenu={handleCreateMenu} />
-    </Container>
+        <NextButton path="/output" name="トレーニング開始" handleCreateMenu={handleCreateMenu} />
+      </Container>
+    </Box>
   );
 };
 
