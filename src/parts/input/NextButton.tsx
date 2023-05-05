2ccea02efe27547564cc36afc25de3f1e4099532
kohki miyama <kmx.111010@gmail.com>
2023-05-05 09:01:19 +0900

    :sparkles: MUIのボタンを使用するように変更 #6

--- src/parts/input/NextButton.tsx
+++ src/parts/input/NextButton.tsx
@@ -1,18 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { Button } from "@mui/material";
+
 interface Props {
   path: string;
   name: string;
-  handleCreateMenu: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  handleCreateMenu: (e: any) => void;
+  handleSendeMenu: (e: React.FormEvent<HTMLButtonElement>) => void;
 }
 
-const NextButton: React.FC<Props> = ({ path, name, handleCreateMenu }) => {
+const NextButton: React.FC<Props> = ({ path, name, handleCreateMenu, handleSendeMenu }) => {
   return (
-    <Link to={`${path}`}>
-      <button type="submit" onClick={handleCreateMenu}>
+    // TODO ボタンが無い部分を推しても反応する
+    <Link to={`${path}`} onClick={handleCreateMenu}>
+      <Button
+        variant="contained"
+        style={{ marginTop: "10%", textAlign: "center" }}
+        onChange={handleSendeMenu}
+      >
         {name}
-      </button>
+      </Button>
     </Link>
   );
 };
